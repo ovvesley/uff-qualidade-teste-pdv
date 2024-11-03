c8ed16c9ddcff82a6af404bccf3a261660dec5c4
laryyssa <paivalaryssa08@gmail.com>
2024-11-03 13:19:40 -0300

    add test merger com empresa existente

--- src/test/java/net/originmobi/pdv/service/EmpresaServiceTest.java
+++ src/test/java/net/originmobi/pdv/service/EmpresaServiceTest.java
@@ -9,10 +9,16 @@ import org.mockito.runners.MockitoJUnitRunner;
 
 import java.util.Optional;
 
+import net.originmobi.pdv.model.Cidade;
+import net.originmobi.pdv.model.EmpresaParametro;
+import net.originmobi.pdv.model.RegimeTributario;
+import net.originmobi.pdv.repository.EmpresaParametrosRepository;
+import net.originmobi.pdv.repository.EmpresaRepository;
+import net.originmobi.pdv.service.EnderecoService;
+import net.originmobi.pdv.service.RegimeTributarioService;
+import net.originmobi.pdv.service.CidadeService;
 import net.originmobi.pdv.model.Empresa;
 import net.originmobi.pdv.model.Endereco;
-import net.originmobi.pdv.model.EmpresaParametro;
-import net.originmobi.pdv.repository.EmpresaRepository;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
@@ -25,6 +31,18 @@ public class EmpresaServiceTest {
 
     @Mock
     private EmpresaRepository empresaRepository;
+
+    @Mock
+    private EmpresaParametrosRepository parametrosRepository;
+
+    @Mock
+    private RegimeTributarioService regimesService;
+
+    @Mock
+    private CidadeService cidadesService;
+
+    @Mock
+    private EnderecoService enderecosService;
 
     private Empresa empresa;
 
@@ -72,6 +90,30 @@ public class EmpresaServiceTest {
         Optional<Empresa> lista_empresas = empresaService.verificaEmpresaCadastrada();
         assertTrue(lista_empresas.isPresent());
         assertEquals("999999999", lista_empresas.get().getCnpj());
+    }
+
+    @Test
+    public void testMergerComEmpresaExistente() {
+        Long codigo = 1L; 
+        String nome = "bagulhos e cia.";
+        String nomeFantasia = "Vendinha";
+        String cnpj = "999999999";
+        String ie = "123456789";
+        int serie = 1;
+        int ambiente = 1;
+        Long codRegime = 1L;
+        Long codEndereco = 1L;
+        Long codCidade = 1L;
+        String rua = "Rua Exemplo";
+        String bairro = "Bairro Exemplo";
+        String numero = "123";
+        String cep = "12345-678";
+        String referencia = "Perto da escola";
+        Double aliqCalcCredito = 5.0;
+
+        String result = empresaService.merger(codigo, nome, nomeFantasia, cnpj, ie, serie, ambiente, codRegime, codEndereco, codCidade, rua, bairro, numero, cep, referencia, aliqCalcCredito);
+
+        assertEquals("Empresa salva com sucesso", result);
     }
 
 }
