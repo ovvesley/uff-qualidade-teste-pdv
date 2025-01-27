e47265a995ef9dce1db697e9583a6074a1da0277
Clara <claralino@id.uff.br>
2025-01-27 13:06:55 -0300

    correção teste 12

--- entrega02/teste_de_sistema/casos_de_teste/caso_12.py
+++ entrega02/teste_de_sistema/casos_de_teste/caso_12.py
@@ -1,3 +1,5 @@
+#Adicionar forma de pagamento
+
 from helpers.usuario import MENU, login
 
 from selenium import webdriver
@@ -10,15 +12,6 @@ from selenium.webdriver.support import expected_conditions as EC
 import time
 
 driver = webdriver.Chrome()
-driver.get("http://localhost:8080")
-
-barra_usuario = driver.find_element(By.ID, "user")
-barra_usuario.send_keys("gerente")
-barra_senha = driver.find_element(By.ID, "password")
-barra_senha.send_keys("123")
-
-botao = driver.find_element(By.ID, "btn-login")
-botao.click()
 
 
 def add_pagamento(descricao, formaPagamento):
