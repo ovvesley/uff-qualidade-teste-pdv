0cfac5dd1821ed9db624dc1c0dfe6b2985153855
laryyssa <paivalaryssa08@gmail.com>
2025-01-27 20:14:13 -0300

    caso 06

--- entrega02/teste_de_sistema/helpers/usuario.py
+++ entrega02/teste_de_sistema/helpers/usuario.py
@@ -1,4 +1,7 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 
 MENU = "http://localhost:8080/login"
 
@@ -11,3 +14,30 @@ def login(driver):
 
     botao = driver.find_element(By.ID, "btn-login")
     botao.click()
+
+def hover_lateral_menu(driver, acao):
+    
+    if acao == "inicio":
+        path_elemento_hover = "/html/body/div[3]/div/div[1]/a/img"
+        path_elemento_click = "/html/body/div[3]/div/div[1]/a"
+
+    elif acao == "pedidos":
+        path_elemento_hover = "/html/body/div[3]/div/div[2]/a/img"
+        path_elemento_click = "/html/body/div[3]/div/div[2]/a"
+    
+    elif acao =="pessoas":
+        path_elemento_hover = "/html/body/div[3]/div/div[8]/a/img"
+        path_elemento_click = "/html/body/div[3]/div/div[8]/a"
+
+    elemento_hover = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, path_elemento_hover))
+    )
+
+    actions = ActionChains(driver)
+    actions.move_to_element(elemento_hover).perform()
+
+    elemento_clicar = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, path_elemento_click))
+    )
+    elemento_clicar.click()
+    
