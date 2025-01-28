aea1515d794c02e63b3cc2792ac8967611d016a6
Clara <claralino@id.uff.br>
2025-01-28 20:37:54 -0300

    corrigindo import de exception

--- entrega02/teste_de_sistema/casos_de_teste/caso_07.py
+++ entrega02/teste_de_sistema/casos_de_teste/caso_07.py
@@ -11,7 +11,8 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
-from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import UnexpectedAlertPresentException
+
 
 driver = webdriver.Chrome()
 
